504129be2f34a4259f960c24053ab1bde141a621
Sami Mäkelä <mrsmkl@gmail.com>
2021-10-14 18:06:50 +0300

    problems here...

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from ctypes import *
 
+print('loading library')
 cdll.LoadLibrary("libkeytransfer.so")
+print('loaded library')
 
 libkey = CDLL('libkeytransfer.so')
 
 libkey.make.restype = c_void_p
 libkey.fullprove.restype = c_char_p
-handle = libkey.make(b"keytransfer.zkey", b"keytransfer.dat")
-res = libkey.fullprove(handle, b"tmp.wtns", b"input.json")
+print('calling make')
+handle = libkey.make(b"/usr/local/share/keytransfer/keytransfer.zkey", b"/usr/local/share/keytransfer/keytransfer.dat")
+print('calling fullprove')
+res = libkey.fullprove(handle, b"/tmp/tmp.wtns", b"/tmp/input.json")
 print(res)
