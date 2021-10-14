42e9bc430d881f6c05d7d0a2ff9450e2367a2f5f
Sami Mäkelä <mrsmkl@gmail.com>
2021-10-14 18:50:35 +0300

    trying to pinpoint error

---
+++ test3.py
@@ -0,0 +1,17 @@
+print('hmmmmm')
+
+from ctypes import *
+
+print('loading library')
+cdll.LoadLibrary("libkeytransfer.so")
+print('loaded library')
+
+libkey = CDLL('libkeytransfer.so')
+
+libkey.make.restype = c_void_p
+libkey.fullprove.restype = c_char_p
+print('calling make')
+handle = libkey.make(b"/usr/local/share/keytransfer/keytransfer.zkey", b"/usr/local/share/keytransfer/keytransfer.dat")
+#print('calling fullprove')
+#libkey.fullprove(handle, b"tmp.wtns", b"input.json")
+#print(res)
