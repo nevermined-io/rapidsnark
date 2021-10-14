219a6905b828729eef575ebda4cba126a9150060
Sami Mäkelä <mrsmkl@gmail.com>
2021-10-14 18:47:44 +0300

    test next step

--- test2.py
+++ test2.py
@@ -9,9 +9,9 @@ print('loaded library')
 libkey = CDLL('libkeytransfer.so')
 
 libkey.make.restype = c_void_p
-libkey.fullprove.restype = c_char_p
+# libkey.fullprove.restype = c_char_p
 print('calling make')
 handle = libkey.make(b"/usr/local/share/keytransfer/keytransfer.zkey", b"/usr/local/share/keytransfer/keytransfer.dat")
-#print('calling fullprove')
-#res = libkey.fullprove(handle, b"/tmp/tmp.wtns", b"input.json")
+print('calling fullprove')
+libkey.fullprove(handle, b"tmp.wtns", b"input.json")
 #print(res)
