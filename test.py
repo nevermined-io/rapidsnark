3f0966df4f9aa1e58eefb5ea8c0293c8a03e6c3a
Sami Mäkelä <mrsmkl@gmail.com>
2021-10-14 17:57:01 +0300

    python test should output something

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ cdll.LoadLibrary("libkeytransfer.so")
 libkey = CDLL('libkeytransfer.so')
 
 libkey.make.restype = c_void_p
+libkey.fullprove.restype = c_char_p
 handle = libkey.make(b"keytransfer.zkey", b"keytransfer.dat")
-libkey.fullprove(handle, b"tmp.wtns", b"input.json")
-
+res = libkey.fullprove(handle, b"tmp.wtns", b"input.json")
+print(res)
