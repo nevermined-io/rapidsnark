c575667a420ff067474e935bf1ece1f182379a5f
Sami Mäkelä <mrsmkl@gmail.com>
2021-10-14 19:22:58 +0300

    testing

--- src/capi.cpp
+++ src/capi.cpp
@@ -249,6 +249,7 @@ extern "C" {
         dta->prover = prover;
         // std::cerr << "Made prover " << (uint64_t)dta << "\n";
         dta->calcwit = ctx;
+        std::cerr << "witness address " << (int)dta->calcwit << "\n";
         return (void*)dta;
     }
 
@@ -256,7 +257,7 @@ extern "C" {
         my_data *dta = (my_data*)ptr;
         std::string wtnsFilename = _wtns;
         std::string inFilename = _in;
-        std::cerr << wtnsFilename << " " << inFilename << "\n";
+        std::cerr << wtnsFilename << " " << inFilename << " " << (int)dta->calcwit << "\n";
 
         /*
         dta->calcwit->reset();
