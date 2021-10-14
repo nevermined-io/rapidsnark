a7b1dafd2f337b873c6517dddad4ffe8cff8f8c4
Sami Mäkelä <mrsmkl@gmail.com>
2021-10-14 18:20:04 +0300

    probably this will do something..

---
+++ test2.py
@@ -0,0 +1,17 @@
+print('hmmmmm')
+
+from ctypes import *
+
+print('loading library')
+#cdll.LoadLibrary("libkeytransfer.so")
+#print('loaded library')
+
+#libkey = CDLL('libkeytransfer.so')
+
+#libkey.make.restype = c_void_p
+#libkey.fullprove.restype = c_char_p
+#print('calling make')
+#handle = libkey.make(b"/usr/local/share/keytransfer/keytransfer.zkey", b"/usr/local/share/keytransfer/keytransfer.dat")
+#print('calling fullprove')
+#res = libkey.fullprove(handle, b"/tmp/tmp.wtns", b"/tmp/input.json")
+#print(res)
