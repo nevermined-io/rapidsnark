84d750f6afdfb9af7de0497b992f511a731194a9
Sami Mäkelä <mrsmkl@gmail.com>
2021-10-14 18:39:56 +0300

    small differences

--- test2.py
+++ test2.py
@@ -6,7 +6,7 @@ print('loading library')
 cdll.LoadLibrary("libkeytransfer.so")
 print('loaded library')
 
-#libkey = CDLL('libkeytransfer.so')
+libkey = CDLL('libkeytransfer.so')
 
 #libkey.make.restype = c_void_p
 #libkey.fullprove.restype = c_char_p
