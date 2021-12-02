fcbf3e58687ee7517000437a007a85a9a0e49b97
Sami Mäkelä <mrsmkl@gmail.com>
2021-12-02 16:13:52 +0200

    install both

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -2,36 +2,30 @@
 
 if ! grep adx /proc/cpuinfo > /dev/null
 then
-    git clone https://github.com/mrsmkl/snark-tools
-    cd snark-tools
-    npm i -g
-    cd ..
-    sudo apt-get -y install g++ build-essential
-    sudo mkdir -p /usr/local/lib && sudo mkdir -p /usr/local/include && sudo mkdir -p /usr/local/share/keytransfer
-    g++ -shared -o alt/libkeytransfer.so -fPIC alt/capi.cpp
-    sudo cp alt/libkeytransfer.so /usr/local/lib/libkeytransfer.so
-    sudo cp keytransfer.zkey keytransfer.dat keytransfer.wasm /usr/local/share/keytransfer
-    sudo ldconfig
+    echo "No Intel ADX detected"
 else
-    sudo apt-get -y install g++ nasm libgmp-dev libsodium-dev build-essential
-    sudo mkdir -p /usr/local/lib && sudo mkdir -p /usr/local/include && sudo mkdir -p /usr/local/share/keytransfer
-    git clone https://github.com/brainhub/SHA3IUF
-    cd SHA3IUF && make && sudo make install && cd ..
-    rm -rf SHA3IUF
-    git submodule update --init --recursive
-    npm i 
-    npx task createFieldSources
-    cd build
-    sed -i 's/extern Fq_fail/;extern Fq_fail/g' *.asm
-    sed -i 's/extern Fr_fail/;extern Fr_fail/g' *.asm
-    sed -i 's/call    Fq_fail/;call    Fq_fail/g' *.asm
-    sed -i 's/call    Fr_fail/;call    Fr_fail/g' *.asm
-    nasm -felf64 fq.asm
-    nasm -felf64 fr.asm
-    cd ..
-    npx task buildApi
-    sudo cp build/libkeytransfer.so /usr/local/lib/libkeytransfer.so
-    sudo cp keytransfer.zkey keytransfer.dat keytransfer.wasm /usr/local/share/keytransfer
-    sudo ldconfig
-
+    echo "Intel ADX detected"
 fi
+
+sudo apt-get -y install g++ nasm libgmp-dev libsodium-dev build-essential
+sudo mkdir -p /usr/local/lib && sudo mkdir -p /usr/local/include && sudo mkdir -p /usr/local/share/keytransfer
+git clone https://github.com/brainhub/SHA3IUF
+cd SHA3IUF && make && sudo make install && cd ..
+rm -rf SHA3IUF
+git submodule update --init --recursive
+npm i 
+npx task createFieldSources
+cd build
+sed -i 's/extern Fq_fail/;extern Fq_fail/g' *.asm
+sed -i 's/extern Fr_fail/;extern Fr_fail/g' *.asm
+sed -i 's/call    Fq_fail/;call    Fq_fail/g' *.asm
+sed -i 's/call    Fr_fail/;call    Fr_fail/g' *.asm
+nasm -felf64 fq.asm
+nasm -felf64 fr.asm
+cd ..
+npx task buildApi
+g++ -shared -o alt/libkeytransfer.so -fPIC alt/capi.cpp
+sudo cp alt/libkeytransfer.so /usr/local/lib/libkeytransfer_noadx.so
+sudo cp build/libkeytransfer.so /usr/local/lib/libkeytransfer.so
+sudo cp keytransfer.zkey keytransfer.dat keytransfer.wasm /usr/local/share/keytransfer
+sudo ldconfig
